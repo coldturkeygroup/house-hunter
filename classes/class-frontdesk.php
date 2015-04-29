7c43aeb39f6e29eab59c7f5983b4b34114290323
Aaron Huisinga <aaron@coldturkeygroup.com>
2015-04-28 22:51:42 -0500

    Bump to version 1.2.6
    1. Add support for new FrontDesk Campaign types.

--- classes/class-frontdesk.php
+++ classes/class-frontdesk.php
@@ -52,6 +52,7 @@ class FrontDesk {
 						'key'         => $this->api_key,
 						'title'       => $title,
 						'description' => 'Campaign for Platform House Hunter',
+						'type'        => 'Platform',
 						'total_cost'  => '10000',
 						'source'      => $permalink
 					]
